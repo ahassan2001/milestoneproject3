82c7ce74bd44fbef5147c819fbc29ffbbca9dfca
unknown <ahassan2001@msn.com>
2024-03-21 23:16:10 -0400

    created config.js

---
+++ backend/config/config.js
@@ -0,0 +1,4 @@
+module.exports = {
+    secretKey: 'Xmankillerfan1!',
+    mongoURI: 'mongodb+srv://ah4487855:<password>@ahmed.6nkfejd.mongodb.net/?retryWrites=true&w=majority&appName=Ahmedmongodb+srv://ah4487855:ZinabHassan2009!@ahmed.6nkfejd.mongodb.net/?retryWrites=true&w=majority&appName=Ahmed'
+};
