803b1038b7ab9bbf01028ed53d0c09acabf6c063
unknown <ahassan2001@msn.com>
2024-03-21 23:38:29 -0400

    RecipeList.js

---
+++ frontend/frontend/src/components/RecipeList.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const RecipeList = ({ recipes }) => {
+  return (
+    <ul>
+      {recipes.map(recipe => (
+        <li key={recipe._id}>{recipe.title}</li>
+      ))}
+    </ul>
+  );
+};
+
+export default RecipeList;
