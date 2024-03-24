f63b7e007b4cfd8a417ccaaa3e86330e5e8890e0
unknown <ahassan2001@msn.com>
2024-03-24 12:41:02 -0400

    updated RecipeList.js

--- frontend/frontend/src/components/RecipeList.js
+++ frontend/frontend/src/components/RecipeList.js
@@ -1,12 +1,33 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
-const RecipeList = ({ recipes }) => {
+const RecipeList = () => {
+  const [recipes, setRecipes] = useState([]);
+
+  useEffect(() => {
+    const fetchRecipes = async () => {
+      const API_KEY = 'c15c17cf69c64b32a1cb8b9ae9afbcb2';
+      const url = 'https://api.spoonacular.com/recipes/complexSearch';
+      try {
+        const response = await axios.get(url);
+        setRecipes(response.data);
+      } catch (error) {
+        console.log('Failed to fetch recipes:', error);
+      }
+    };
+
+    fetchRecipes();
+  }, []);
+
   return (
-    <ul>
-      {recipes.map(recipe => (
-        <li key={recipe._id}>{recipe.title}</li>
-      ))}
-    </ul>
+    <div>
+      <h2>Recipes</h2>
+      <ul>
+        {recipes.map((recipe) => (
+          <li key={recipe.id}>{recipe.title}</li>
+        ))}
+      </ul>
+    </div>
   );
 };
 
